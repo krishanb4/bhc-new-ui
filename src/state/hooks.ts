489dce1300d84b1f2a0805086ed16ec9984d74fb
crezix <52518140+crezix@users.noreply.github.com>
2022-03-28 21:17:25 +0530

    avax pools 1

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -31,15 +31,18 @@ export const useFetchPublicData = () => {
   const { slowRefresh } = useRefresh()
   useEffect(() => {
     dispatch(fetchFarmsPublicDataAsync())
-    fetch('https://bsctools.xyz/bhc/api/bhc_lp_calc.php')
+    fetch('https://bsctools.xyz/bhc/api/bhc_lp_calc_avax.php')
       .then((res) => res.text())
       .then((body) => {
-        fetch('https://bsctools.xyz/bhc/api/bhc_price.php')
+        fetch('https://bsctools.xyz/bhc/api/bhc_price_avax.php')
           .then((res1) => res1.text())
           .then((body1) => {
             window.prices = {
-              '0xa8752333f6a6fe47323a4eDAC3D09Bb1048A0E23': JSON.parse(body1).bhc_streetswap, // BHC
-              '0x695Fa794d59106cEbd40ab5f5cA19F458c723829': 0, // HAKU
+              '0xa8752333f6a6fe47323a4eDAC3D09Bb1048A0E23': JSON.parse(body1).bhc, // BHC
+              '0x695Fa794d59106cEbd40ab5f5cA19F458c723829': JSON.parse(body1).haku, // HAKU
+              '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7': JSON.parse(body1).avax, // AVAX
+
+              '0x014C13e4e734b90cDe56f96F42E7227Fc2b2e86E': JSON.parse(body).bhcavax_one_lp_value, ///haku lp bhc-avax
             }
           })
       })
